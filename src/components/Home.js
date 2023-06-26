7b29b3ff43b190448bb420a61cb11c60cc732b63
Melanie Wilson <melaniemwilson1@gmail.com>
2023-06-26 16:53:33 -0400

    updated home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,19 @@
 import React from "react"
+import { Image, Slider } from 'semantic-ui-react'
 
 function Home(){
+
+    // const images = [
+    //     "https://images.unsplash.com/photo-1503676260728-1c00da094a0b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1722&q=80",
+    //     "https://images.unsplash.com/photo-1532619187608-e5375cab36aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+    //     "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+    //     "https://images.unsplash.com/photo-1580894732444-8ecded7900cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+    // ]
+    
     return(
         <div>
             <h1>Unlocking Academic Success Through Personalized Tutoring.</h1>
+            <Image src="https://images.unsplash.com/photo-1503676260728-1c00da094a0b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1722&q=80" alt="homepage-image" size="huge" bordered centered/>
         </div>
     )
 }
