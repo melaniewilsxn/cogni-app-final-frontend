36dbf8cc8176336c91a4e006791cc6e8754cc9fc
Melanie Wilson <melaniemwilson1@gmail.com>
2023-06-27 12:11:17 -0400

    updated db.json

--- src/components/TutorCard.js
+++ src/components/TutorCard.js
@@ -1,16 +1,36 @@
-import React from "react"
-import { Card } from "semantic-ui-react";
+import React, { useState } from "react"
+import { Card, Image, Button } from "semantic-ui-react";
 
 function TutorCard({tutor}){
 
+    const [tutorClicked, setTutorClicked] = useState(false)
+
+    function handleClick(){
+        setTutorClicked(!tutorClicked)
+    }
+
     return(
         <Card>
-            <img className= "image" src={tutor.image} alt={tutor.name}/>
-            <div className="content">
-                <h2>{tutor.name}</h2>
-            </div>
+            <Image src={tutor.image} wrapped ui={false} />
+            <Card.Content>
+                <Card.Header>{tutor.name}</Card.Header>
+                <Card.Meta>{tutor.subject}</Card.Meta>
+                {tutorClicked ? 
+                <Card.Description>
+                    Daniel is a comedian living in Nashville.
+                </Card.Description>
+                : null}
+            </Card.Content>
+            <Button onClick={handleClick}>See More</Button>
         </Card>
     )
 }
 
 export default TutorCard;
+
+{/* <Card>
+<img className= "image" src={tutor.image} alt={tutor.name}/>
+<div className="content">
+    <h2>{tutor.name}</h2>
+</div>
+</Card> */}
