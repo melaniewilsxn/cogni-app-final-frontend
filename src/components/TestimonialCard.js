ea2a5206df47085e4b1a8bb448d02c24c4277559
Melanie Wilson <melaniemwilson1@gmail.com>
2023-06-27 12:35:47 -0400

    updated testimonial componenets

---
+++ src/components/TestimonialCard.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Header, Divider } from "semantic-ui-react";
+
+function TestimonialCard({testimonial}){
+
+    return(
+        <div>
+            <Header as='h3'>{testimonial.first_name} {testimonial.last_initial}</Header>
+            <p>{testimonial.testimonial}</p>
+            
+            <Divider section />
+        </div>
+    )
+}
+
+export default TestimonialCard;
--- src/components/TestimonialList.js
+++ src/components/TestimonialList.js
@@ -1,32 +1,29 @@
 import React, { useState, useEffect} from 'react'
-import { Card, Divider } from 'semantic-ui-react'
+import { Container, Divider, Segment } from 'semantic-ui-react'
+import TestimonialCard from "./TestimonialCard"
 
 function TestimonialList(){
 
-    // const [tutorList, setTutorList] = useState([])
+    const [testimonialList, setTestimonialList] = useState([])
 
-    // useEffect(() => {
-    //     fetch("http://localhost:3001/tutors")
-    //     .then(res  => res.json())
-    //     .then(data => setTutorList(data))
-    // }, [])
+    useEffect(() => {
+        fetch("http://localhost:3001/testimonials")
+        .then(res  => res.json())
+        .then(data => setTestimonialList(data))
+    }, [])
 
-    // const displayTutors = tutorList.map((tutor) => {
-    //     return <TutorCard tutor={tutor} key={tutor.id}/>
-    // })
+    const displayTestimonials = testimonialList.map((testimonial) => {
+        return <TestimonialCard testimonial={testimonial} key={testimonial.id}/>
+    })
     
     return(
-        <div>
+        <Container>
             <h1>Testimonials</h1>
-            {/* <Divider />
-            <p>
-            At Cogni, we prioritize building strong student-tutor relationships as the foundation of our work. We are committed to selecting only the most highly qualified tutors who possess a bachelor's degree or higher. We firmly believe that effective teaching requires not only intellectual ability and enthusiasm but also maturity and experience. Our tutors bring a wealth of subject expertise, often holding advanced degrees in their respective disciplines, coupled with a genuine passion for the subjects they teach. This unique combination of deep subject knowledge and extensive teaching experience enables our tutors to excel in their role. Many of them have valuable experience as seasoned classroom teachers, while others have dedicated themselves to volunteering or teaching outside traditional classroom settings. Above all, our tutors have a remarkable track record of success in improving student performance. We take immense pride in presenting our exceptional team of tutors who are dedicated to guiding students towards academic excellence.
-            </p>
             <Divider />
-            <Card.Group centered>
-                {displayTutors}
-            </Card.Group> */}
-        </div>
+            <Segment textAlign="left">
+                {displayTestimonials}
+            </Segment>
+        </Container>
     )
 }
 
